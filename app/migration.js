91b6e177c4707a415373b8c3ec6695aaaa85ab7c
Teerapath Sujitto <teerapart.s@gmail.com>
2017-05-07 14:40:30 +0700

    Demo migration

--- app/migration.js
+++ app/migration.js
@@ -1,4 +1,5 @@
 const sshClient = require('ssh2').Client;
+const bl = require('bl');
 var jobs = [];
 
 exports.registerJob = function(sshSrc,sshDes){
@@ -37,8 +38,13 @@ exports.registerJob = function(sshSrc,sshDes){
     .catch(console.log)
 }
 
+var escapeShell = function(cmd) {
+  return '"'+cmd.replace(/(["\s'$`\\])/g,'\\$1')+'"';
+};
+
 exports.migrate = function(){
     console.log("Migration started");
+    //Repetitive code fixed it later . . .
     function dumpNode(sshOption,pid){
         var conn = new sshClient();
         return new Promise(function(resolve,reject){
@@ -48,7 +54,9 @@ exports.migrate = function(){
                 ,{pty:true}, function(err, stream) {
                     if (err) throw err;
                     stream.on('close',()=>{
+                        console.log("stream end");
                         conn.end();
+                        resolve();
                     })
                     stream.on('data', function(data) {
                         data = data.toString();
@@ -67,17 +75,19 @@ exports.migrate = function(){
         var conn = new sshClient();
         return new Promise(function(resolve,reject){
             conn.on('ready',function(){
-                conn.exec(`scp -r ~/img ${src.username}@${src.host}:~`
+                console.log("Transfering img file")
+                conn.exec(`scp -r ~/img ${src.username}@${des.host}:~`
                 ,{pty:true}, function(err, stream) {
                     if (err) throw err;
                     stream.on('close',()=>{
+                        console.log("stream end");
                         conn.end();
+                        resolve();
                     })
                     stream.on('data', function(data) {
                         data = data.toString();
-                        console.log(data.toString());
-                        if(data.indexOf('password for')>-1)return stream.write(sshOption.password + '\n')
-                         
+                        process.stdout.write(data);
+                        if(data.indexOf('password')>-1)return stream.write(des.password + '\n')
                     }).stderr.on('data', function(data) {
                         console.log('STDERR: ' + data);
                         return reject(data.toString());
@@ -86,15 +96,95 @@ exports.migrate = function(){
             }).connect(src)
         }).catch(console.log)
     }
-    function restoreNode(sshOption,pid){
+    function readInetsk(sshOption){
+        var conn = new sshClient();
+        return new Promise(function(resolve,reject){
+            conn.on('ready', function() {
+                console.log("Decoding inetsk");
+                conn.exec(`crit decode -i ~/img/inetsk.img --pretty`
+                ,{pty:true}, function(err, stream) {
+                    if (err) throw err;
+                    stream.pipe(bl(function(err,data){
+                        if(err)return reject(err)
+                        return resolve(JSON.parse(data.toString()));
+                    }));
+                });
+            }).connect(sshOption);      
+        }).catch(console.log)  
+    }
 
+    function modifyInetsk(sshOption,inetsk){
+        var conn = new sshClient();
+        return new Promise(function(resolve,reject){
+            conn.on('ready',function(){
+                console.log("Encoding crit");
+                conn.exec(`echo ${escapeShell(inetsk)} | sudo crit encode -o ~/img/inetsk.img`
+                ,{pty:true}, function(err, stream) {
+                    if (err) throw err;
+                    stream.on('data',(data)=>{
+                        data = data.toString();
+                        console.log(data);
+                        if(data.indexOf('password')>-1)return stream.write(sshOption.password + '\n')
+                    })
+                    stream.on('close',()=>{
+                        console.log("stream end");
+                        conn.end();
+                        resolve();
+                    }).stderr.on('data', function(data) {
+                        console.log('STDERR: ' + data);
+                        return reject(data.toString());
+                    });
+                });
+            }).connect(sshOption)
+        }).catch(console.log)
+    }
+    function restoreNode(sshOption,pid){
+        var conn = new sshClient();
+        return new Promise(function(resolve,reject){
+            conn.on('ready', function() {
+                console.log("Restoring " + pid);
+                conn.exec(`sudo criu restore -t ${pid} --images-dir ~/img --tcp-established  --shell-job --ext-unix-sk`
+                ,{pty:true}, function(err, stream) {
+                    if (err) throw err;
+                    stream.on('close',()=>{
+                        console.log("stream end");
+                        resolve();
+                        conn.end();
+                    })
+                    stream.on('error',console.log);
+                    stream.on('data', function(data) {
+                        data = data.toString();
+                        process.stdout.write(data);
+                        if(data.indexOf('password for')>-1)return stream.write(sshOption.password + '\n');
+                         
+                    }).stderr.on('data', function(data) {
+                        console.log('STDERR: ' + data);
+                        return reject(data.toString());
+                    });
+                });
+            }).connect(sshOption);      
+        }).catch(console.log)
     }
     return new Promise(function(resolve,reject){
         jobs.forEach(job=>{
             dumpNode(job.src,job.pid)
             .then(()=>{
+                console.log("Changing IP in inetsk")
+                return readInetsk(job.src)
+            })
+            .then(inetsk=>{
+                inetsk.entries = inetsk.entries.map(entry=>{
+                    if(entry.src_addr[0] == job.src.host)entry.src_addr[0] = job.des.host
+                    return entry;
+                })
+                console.log("New inetsk \n",JSON.stringify(inetsk));
+                return modifyInetsk(job.src,JSON.stringify(inetsk));
+            }).then(()=>{
+                console.log("wtf");
                 return copyImg(job.src,job.des)
-            });
+            }).then(()=>{
+                return restoreNode(job.des,job.pid);
+            })
         });
     })
 }
